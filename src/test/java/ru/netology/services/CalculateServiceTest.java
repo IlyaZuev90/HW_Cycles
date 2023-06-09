711edc6557565f86f8277e532107500ff6911257
Ilya_Zuev_90 <god_of_iwowi@mail.ru>
2023-06-09 22:39:09 +0400

    Second task completed - CSV file added

--- src/test/java/ru/netology/services/CalculateServiceTest.java
+++ src/test/java/ru/netology/services/CalculateServiceTest.java
@@ -2,15 +2,19 @@ package ru.netology.services;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
 
 public class CalculateServiceTest {
 
-    @Test
-    public void testCalculateLowIncomeLowExpensesLowThreshold() {
+    @ParameterizedTest
+    @CsvFileSource(files = "src/test/resources/ParameterListForCalculations.csv")
+    public void testCalculate(int expected, int income, int expenses, int threshold) {
         CalculateService service = new CalculateService();
 
-        int expected = 3;
-        int actual = service.calculate(10_000, 3_000, 20_000);
+        //int expected = 3;
+        int actual = service.calculate(income, expenses, threshold);
 
         Assertions.assertEquals(expected, actual);
         System.out.println();
@@ -18,16 +22,16 @@ public class CalculateServiceTest {
         System.out.println();
     }
 
-    @Test
-    public void testCalculateHighIncomeHighExpensesHighThreshold() {
-        CalculateService service = new CalculateService();
-
-        int expected = 2;
-        int actual = service.calculate(100_000, 60_000, 150_000);
-
-        Assertions.assertEquals(expected, actual);
-        System.out.println();
-        System.out.println("Количество месяцев для отдыха: " + actual);
-        System.out.println();
-    }
+//    @Test
+//    public void testCalculateHighIncomeHighExpensesHighThreshold() {
+//        CalculateService service = new CalculateService();
+//
+//        int expected = 2;
+//        int actual = service.calculate(100_000, 60_000, 150_000);
+//
+//        Assertions.assertEquals(expected, actual);
+//        System.out.println();
+//        System.out.println("Количество месяцев для отдыха: " + actual);
+//        System.out.println();
+//    }
 }
